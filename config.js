7f8a15d3feb781c691f567f4dc6d21013e5b97df
Nguyễn Đức Bảo <119411591+blue552@users.noreply.github.com>
2024-11-09 00:14:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Trần Kim Oanh';
+const giftUrl = 'https://www.facebook.com/kimoanh.tranthuy.37';
+const eventName = 'Happy Birthday';
+const titleCard = 'From: Nguyễn Đức Bảo';
+const contentCard = 'Chúc mừng sinh nhật bạn yêu! Chúc bạn luôn rạng rỡ như ánh mặt trời, tràn đầy năng lượng hạt nhân. Hy vọng tuổi mới sẽ mang đến cho bạn nhiều trải nghiệm đáng nhớ, thành công và hạnh phúc ngập tràn. ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
